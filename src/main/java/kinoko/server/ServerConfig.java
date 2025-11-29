327ab475d96a0eaccaf47581cf32488d0bc33215
MujyKun <mujy@irenebot.com>
2025-11-28 20:53:27 -0500

    Added gender lock

--- src/main/java/kinoko/server/ServerConfig.java
+++ src/main/java/kinoko/server/ServerConfig.java
@@ -37,6 +37,7 @@ public final class ServerConfig {
     public static final boolean DEBUG_MODE = Util.getEnv("DEBUG_MODE", true);
     public static final boolean PLAIN_TRAFFIC = Util.getEnv("PLAIN_TRAFFIC", false);
 
+    public static final boolean GENDER_LOCK = Util.getEnv("GENDER_LOCK", true);
 
     public static void setExpRate(int rate) {
         EXP_RATE = Math.max(1, rate);
